9c3539f291b077bcef5393a37f5dce2bc1d2c74f
alemages <alex.mages@lptech.com>
2021-11-16 15:57:08 -0600

    Added toFixed to add, sub, mult, and divide.

--- src/hertz.js
+++ src/hertz.js
@@ -7,22 +7,22 @@ export function toHz(freq, toDec) {
   return formatFreqCheck(freq, units, toDec);
 }
 
-export function toKiloHz(freq, toDec) {
+export function toKiloHz(freq, toDec = 8) {
   const units = unitsArr[1];
   return formatFreqCheck(freq, units, toDec);
 }
 
-export function toMegaHz(freq, toDec) {
+export function toMegaHz(freq, toDec = 8) {
   const units = unitsArr[2];
   return formatFreqCheck(freq, units, toDec);
 }
 
-export function toGigaHz(freq, toDec) {
+export function toGigaHz(freq, toDec = 8) {
   const units = unitsArr[3];
   return formatFreqCheck(freq, units, toDec);
 }
 
-export function toTeraHz(freq, toDec) {
+export function toTeraHz(freq, toDec = 8) {
   const units = unitsArr[4];
   return formatFreqCheck(freq, units, toDec);
 }
@@ -43,95 +43,95 @@ export function convertFrequency(freq, toUnits) {
   }
 }
 
-export function addFreq(pFreq, sFreq) {
-  return mathAddFormatCheck(pFreq, sFreq);
+export function addFreq(pFreq, sFreq, toDec = 8) {
+  return mathAddFormatCheck(pFreq, sFreq, toDec);
 }
 
-export function subFreq(pFreq, sFreq) {
-  return mathSubFormatCheck(pFreq, sFreq);
+export function subFreq(pFreq, sFreq, toDec = 8) {
+  return mathSubFormatCheck(pFreq, sFreq, toDec);
 }
 
-export function multFreq(freq, multiplier) {
-  return mathMulFormatCheck(freq, multiplier);
+export function multFreq(freq, multiplier, toDec = 8) {
+  return mathMulFormatCheck(freq, multiplier, toDec);
 }
 
-export function divFreq(freq, divisor, toDec = 0) {
+export function divFreq(freq, divisor, toDec = 8) {
   return mathDivFormatCheck(freq, divisor, toDec);
 }
 
-function mathAddFormatCheck(pFreq, sFreq) {
+function mathAddFormatCheck(pFreq, sFreq, toDec) {
   if (typeof pFreq === 'number' && typeof sFreq === 'number') {
-    return toHz(pFreq + sFreq);
+    return toHz(pFreq + sFreq, toDec);
   } else if (typeof pFreq === 'number' && typeof sFreq === 'string') {
     const pFreqUnit = toHz(pFreq);
-    return addFreqMath(pFreqUnit, sFreq);
+    return addFreqMath(pFreqUnit, sFreq, toDec);
   } else if (typeof pFreq === 'string' && typeof sFreq === 'number') {
-    return addFreqMath(pFreq, toHz(sFreq));
+    return addFreqMath(pFreq, toHz(sFreq), toDec);
   } else if (typeof pFreq === 'string' && typeof sFreq === 'string') {
-    return addFreqMath(pFreq, sFreq);
+    return addFreqMath(pFreq, sFreq, toDec);
   } else {
     throw new Error('string not valid');
   }
 }
 
-function addFreqMath(pFreq, sFreq) {
+function addFreqMath(pFreq, sFreq, toDec) {
   const pHzUnit = freqParser(pFreq);
   const pHz = parseFloat(toHz(pFreq));
   const sHz = parseFloat(toHz(sFreq));
   const addedFreq = pHz + sHz;
   switch (pHzUnit[1]) {
     case 0:
-      return toHz(addedFreq, pHzUnit[1]);
+      return toHz(addedFreq, toDec);
     case 1:
-      return toKiloHz(addedFreq, pHzUnit[1]);
+      return toKiloHz(addedFreq, toDec);
     case 2:
-      return toMegaHz(addedFreq, pHzUnit[1]);
+      return toMegaHz(addedFreq, toDec);
     case 3:
-      return toGigaHz(addedFreq, pHzUnit[1]);
+      return toGigaHz(addedFreq, toDec);
     case 4:
-      return toTeraHz(addedFreq, pHzUnit[1]);
+      return toTeraHz(addedFreq, toDec);
   }
 }
 
-function mathSubFormatCheck(pFreq, sFreq) {
+function mathSubFormatCheck(pFreq, sFreq, toDec) {
   if (typeof pFreq === 'number' && typeof sFreq === 'number') {
-    return toHz(pFreq - sFreq);
+    return toHz(pFreq - sFreq, toDec);
   } else if (typeof pFreq === 'number' && typeof sFreq === 'string') {
     const pFreqUnit = toHz(pFreq);
-    return subFreqMath(pFreqUnit, sFreq);
+    return subFreqMath(pFreqUnit, sFreq, toDec);
   } else if (typeof pFreq === 'string' && typeof sFreq === 'number') {
-    return subFreqMath(pFreq, toHz(sFreq));
+    return subFreqMath(pFreq, toHz(sFreq), toDec);
   } else if (typeof pFreq === 'string' && typeof sFreq === 'string') {
-    return subFreqMath(pFreq, sFreq);
+    return subFreqMath(pFreq, sFreq, toDec);
   } else {
     throw new Error('string not valid');
   }
 }
 
-function subFreqMath(pFreq, sFreq) {
+function subFreqMath(pFreq, sFreq, toDec) {
   const pHzUnit = freqParser(pFreq);
   const pHz = parseFloat(toHz(pFreq));
   const sHz = parseFloat(toHz(sFreq));
   const addedFreq = pHz - sHz;
   switch (pHzUnit[1]) {
     case 0:
-      return toHz(addedFreq, pHzUnit[1]);
+      return toHz(addedFreq, toDec);
     case 1:
-      return toKiloHz(addedFreq, pHzUnit[1]);
+      return toKiloHz(addedFreq, toDec);
     case 2:
-      return toMegaHz(addedFreq, pHzUnit[1]);
+      return toMegaHz(addedFreq, toDec);
     case 3:
-      return toGigaHz(addedFreq, pHzUnit[1]);
+      return toGigaHz(addedFreq, toDec);
     case 4:
-      return toTeraHz(addedFreq, pHzUnit[1]);
+      return toTeraHz(addedFreq, toDec);
   }
 }
 
-function mathMulFormatCheck(freq, multiplier) {
+function mathMulFormatCheck(freq, multiplier, toDec) {
   if (typeof freq === 'number') {
-    return toHz(freq * multiplier);
+    return toHz(freq * multiplier, toDec);
   } else if (typeof freq === 'string') {
-    return multFreqMath(freq, multiplier);
+    return multFreqMath(freq, multiplier, toDec);
   }
 }
 
@@ -143,39 +143,39 @@ function mathDivFormatCheck(freq, divisor, toDec) {
   }
 }
 
-function multFreqMath(freq, multipler) {
+function multFreqMath(freq, multipler, toDec) {
   const freqUnit = freqParser(freq);
   const freqVal = parseFloat(toHz(freq));
   const multFreq = freqVal * multipler;
   switch (freqUnit[1]) {
     case 0:
-      return toHz(multFreq, freqUnit[1]);
+      return toHz(multFreq, toDec);
     case 1:
-      return toKiloHz(multFreq, freqUnit[1]);
+      return toKiloHz(multFreq, toDec);
     case 2:
-      return toMegaHz(multFreq, freqUnit[1]);
+      return toMegaHz(multFreq, toDec);
     case 3:
-      return toGigaHz(multFreq, freqUnit[1]);
+      return toGigaHz(multFreq, toDec);
     case 4:
-      return toTeraHz(multFreq, freqUnit[1]);
+      return toTeraHz(multFreq, toDec);
   }
 }
 
 function divFreqMath(freq, divisor, toDec) {
   const freqUnit = freqParser(freq);
-  const freqVal = parseFloat(toHz(freq));
+  const freqVal = parseFloat(toHz(freq, toDec));
   const divFreq = freqVal / divisor;
   switch (freqUnit[1]) {
     case 0:
-      return toHz(divFreq, freqUnit[1], toDec);
+      return toHz(divFreq, toDec);
     case 1:
-      return toKiloHz(divFreq, freqUnit[1], toDec);
+      return toKiloHz(divFreq, toDec);
     case 2:
-      return toMegaHz(divFreq, freqUnit[1], toDec);
+      return toMegaHz(divFreq, toDec);
     case 3:
-      return toGigaHz(divFreq, freqUnit[1], toDec);
+      return toGigaHz(divFreq, toDec);
     case 4:
-      return toTeraHz(divFreq, freqUnit[1], toDec);
+      return toTeraHz(divFreq, toDec);
   }
 }
 
@@ -191,8 +191,7 @@ function formatFreqCheck(freq, units, toDec) {
 function toStringFreq(freq, units, toDec) {
   const parsed = freqParser(freq);
   const indexDiff = parsed[1] - unitsArr.indexOf(units);
-  const output = parsed[0] * Math.pow(1000, indexDiff);
-  console.log(toDec);
+  const output = (parsed[0] * Math.pow(1000, indexDiff)).toFixed(toDec);
   return output.toString() + ' ' + unitsCapArr[unitsArr.indexOf(units)];
 }
 
--- src/index.js
+++ src/index.js
@@ -12,7 +12,3 @@ export {
 } from './hertz';
 
 export { toDbm, toDbw, toDbuw, toMw, toUw, toW } from './decibel';
-
-import { divFreq } from './hertz';
-
-console.log(divFreq('500 mhz', 11, 6));
